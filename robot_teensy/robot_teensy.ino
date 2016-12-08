f4f4f21e629fa510f4b1a1831beba95fff814023
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-12-07 19:40:04 -0800

    cleanup

--- robot_teensy/robot_teensy.ino
+++ robot_teensy/robot_teensy.ino
@@ -5,59 +5,45 @@
 #include <PulsePosition.h>
 #include <Adafruit_PWMServoDriver.h>
 
+// Servo driver stuff:
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  560 // this is the 'maximum' pulse length count (out of 4096)
 
-
-
+// PPM decoder stuff:
 PulsePositionInput myIn;
-
-byte v_axis = 2; // selector for vertical axis, 0 = x, 1 = y, 2 = z, -1 = disable inversion check
-
-Adafruit_9DOF                 dof   = Adafruit_9DOF();
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
-Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
-
-boolean inverted = false;
-
 boolean connected = false, firstConnect = false;
-
-
 int axis[] = {0,0,0,0};
 int flap = 0;
 int fm = 0;
 int aux = 0;
-
 int conn_ctr = 0, num = 0;
 
-void initSensors()
-{
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-    while(1);
-  }
-  if(!mag.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while(1);
-  }
-}
+// 9DOF sensor stuff:
+Adafruit_9DOF                 dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+boolean accel_valid = false;
+
+// Robot Orientation stuff:
+byte v_axis = 2; // selector for vertical axis, 0 = x, 1 = y, 2 = z, 3 = disable inversion check
+boolean v_invert = 0;	// Invert the vertical axis. 
+boolean inverted = false;
+
 
 
 void setup() {
-  Serial.begin(115200);
-  delay(50);
-  initSensors();
+  Serial.begin(115200);	// Serial for debugging...
+  delay(50);	// Delay to let devices connect. 
   
-  pwm.begin();
+  // init devices
+  accel_valid = accel.begin();	// Accel
+  pwm.begin();					// PWM Servo Driver
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  yield();
   pinMode(17, OUTPUT);
   digitalWrite(17, LOW);
+  
+  yield();
 
   myIn.begin(6);
 }
@@ -140,13 +126,10 @@ void updateSensors() {
 }
 
 void updateRx() {
-
-  // Every time new data arrives, simply print it
-  // to the Arduino Serial Monitor.
   num = myIn.available();
 
   
-  if (num < 15 && num != -1) {
+  if (num < 15 && num != -1) {	// Read ppm data if valid.
     axis[0] = map(myIn.read(1), 805, 2215, -100, 100)-2;
     axis[1] = map(myIn.read(2), 1100, 1925, -100, 100);
     axis[2] = map(myIn.read(3), 805, 2215, -100, 100)-2;
@@ -161,15 +144,18 @@ void updateRx() {
   
   
 
-  if(conn_ctr > 3) {
+  if(conn_ctr > 3) {	// if non-valid signal for 3 loops, zero inputs.
     connected = false;
     axis[0] = 0;
     axis[1] = 0;
     axis[2] = 0;
     axis[3] = 0;
+	
+	flap = 0;
+	fm = 0;
+	aux = 0;
   } else {
     connected = true;
   }
 } 
-
 
