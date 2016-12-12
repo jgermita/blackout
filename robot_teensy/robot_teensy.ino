086041f6214d6085ab193111c2fb54fdbba6ff2e
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-12-11 21:35:30 -0800

    cleannup, better motor inputs

--- robot_teensy/robot_teensy.ino
+++ robot_teensy/robot_teensy.ino
@@ -9,40 +9,51 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  560 // this is the 'maximum' pulse length count (out of 4096)
+int8_t motor[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+// Motor ID definitions:
+#define LEFT_DRIVE 0
+#define RIGHT_DRIVE 1
+#define WEAPON 2
+#define LED_STRIP 3
 
 // PPM decoder stuff:
 PulsePositionInput myIn;
 boolean connected = false, firstConnect = false;
-int axis[] = {0,0,0,0};
+int8_t axis[] = {0, 0, 0, 0};
 int flap = 0;
 int fm = 0;
 int aux = 0;
 int conn_ctr = 0, num = 0;
+// Axis ID definitions:
+#define THROTTLE 0
+#define YAW 1
+#define ELEVATOR 2
+#define ROLL 3
 
 // 9DOF sensor stuff:
 Adafruit_9DOF                 dof   = Adafruit_9DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 boolean accel_valid = false;
+int accel_data[] = {0, 0, 0};
+int gyro_data[] = {0, 0, 0};
 
 // Robot Orientation stuff:
 byte v_axis = 2; // selector for vertical axis, 0 = x, 1 = y, 2 = z, 3 = disable inversion check
-boolean v_invert = 0;	// Invert the vertical axis. 
+#define v_invert 1
 boolean inverted = false;
-
-
 
 void setup() {
   Serial.begin(115200);	// Serial for debugging...
-  delay(50);	// Delay to let devices connect. 
-  
+  delay(50);	// Delay to let devices connect.
+
   // init devices
   accel_valid = accel.begin();	// Accel
   pwm.begin();					// PWM Servo Driver
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   pinMode(17, OUTPUT);
   digitalWrite(17, LOW);
-  
+
   yield();
 
   myIn.begin(6);
@@ -51,57 +62,48 @@ void setup() {
 }
 
 void loop() {
+  int8_t lOut = 0;
+  int8_t rOut = 0;
+
+  if (!inverted) {
+    lOut = axis[THROTTLE];
+    rOut = axis[ELEVATOR];
+  } else {
+    lOut = -axis[ELEVATOR];
+    rOut = -axis[THROTTLE];
+  }
+
+  motor[LEFT_DRIVE] = lOut;
+  motor[RIGHT_DRIVE] = rOut;
+
+  // "SmartSpin" weapon control. 
+  // pulls back weapon command speed as a function of commanded turning rate
+  int8_t weaponIn = (flap * 50) - 100;
+  
+  int8_t turning = (motor[LEFT_DRIVE] + motor[RIGHT_DRIVE]);
+  motor[WEAPON] = weaponIn - ((turning*4)/5);
+  
+  motor[LED_STRIP] = (fm * 100) - 100;
+  
   updateSensors();
   updateRx();
   updatePwm();
-
-  int lOut = 0;
-  int rOut = 0;
-  
-  if(!inverted) {
-    lOut = axis[0];
-    rOut = axis[2];
-  } else {
-    lOut = -axis[2];
-    rOut = -axis[0];
-  }
-  
-  setPwm(0, lOut);
-  setPwm(1, rOut);
-  setPwm(2, (flap*50)-100);
-  setPwm(3, (fm * 100)-100);
-
-  Serial.print(flap);
-  Serial.print("\t");
-  Serial.print(fm);
-  Serial.print("\t");
-  Serial.print(aux);
-  Serial.println("\t");
-  
-  //Serial.println(inverted ? 1 : 0);
-  Serial2.print(connected);
-  Serial2.print("\t");
-  Serial2.print(num);
-  Serial2.print("\t");
-  Serial2.print(axis[0]);
-  Serial2.print("\t");
-  Serial2.print(axis[1]);
-  Serial2.print("\t");
-  Serial2.print(axis[2]);
-  Serial2.print("\t");
-  Serial2.println(axis[3]);
-  delay(10);
+  updateLog();
+  delay(5);
 }
 
 
 void updatePwm() {
   digitalWrite(17, !connected);
+
+  for(int8_t i = 0; i < 16; i++) {
+    pwm.setPWM(i, 0, map(motor[i], -100, 100, SERVOMIN, SERVOMAX));  
+  }
 }
 
-void setPwm(int id, int value) {
-  pwm.setPWM(id, 0, map(value, -100, 100, SERVOMIN, SERVOMAX));
-}
 
+
+int inv_ctr = 0;
 void updateSensors() {
   sensors_event_t accel_event;
   sensors_event_t mag_event;
@@ -111,16 +113,30 @@ void updateSensors() {
   accel.getEvent(&accel_event);
   mag.getEvent(&mag_event);
 
-  switch(v_axis) {
-    case 0: 
-      inverted = accel_event.acceleration.x < 0; break;
+  accel_data[0] = accel_event.acceleration.x;
+  accel_data[1] = accel_event.acceleration.y;
+  accel_data[2] = accel_event.acceleration.z;
+  int axis = 0;
+  switch (v_axis) {
+    case 0:
+      axis = accel_data[0]; break;
     case 1:
-      inverted = accel_event.acceleration.y < 0; break;
+      axis = accel_data[1]; break;
     case 2:
-      inverted = accel_event.acceleration.z < 0; break;
+      axis = accel_data[2]; break;
   }
+
+  axis *= v_invert;
+
+  if(axis < -7) {
+    inv_ctr++;
+  } else {
+    inv_ctr = 0;
+  }
+
+  inverted = inv_ctr > 2;
   
-  /* Use the new fusionGetOrientation function to merge accel/mag data */  
+  /* Use the new fusionGetOrientation function to merge accel/mag data */
   if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation))
   {
     /* 'orientation' should have valid .roll and .pitch fields */
@@ -132,17 +148,17 @@ void updateSensors() {
     //Serial.print(orientation.heading);
     //Serial.println((""));
   }
-  
+
 }
 
 void updateRx() {
   num = myIn.available();
 
-  
+
   if (num < 15 && num != -1) {	// Read ppm data if valid.
-    axis[0] = map(myIn.read(1), 805, 2215, -100, 100)-2;
+    axis[0] = map(myIn.read(1), 805, 2215, -100, 100) - 2;
     axis[1] = map(myIn.read(2), 1100, 1925, -100, 100);
-    axis[2] = map(myIn.read(3), 805, 2215, -100, 100)-2;
+    axis[2] = map(myIn.read(3), 805, 2215, -100, 100) - 2;
     axis[3] = map(myIn.read(4), 1100, 1925, -100, 100);
     flap = map(myIn.read(5), 801, 2219, 0, 2);
     fm = map(myIn.read(7), 1034, 1985, 0, 2);
@@ -151,21 +167,69 @@ void updateRx() {
   } else {
     conn_ctr++;
   }
-  
-  
 
-  if(conn_ctr > 3) {	// if non-valid signal for 3 loops, zero inputs.
+
+
+  if (conn_ctr > 3) {	// if non-valid signal for 3 loops, zero inputs.
     connected = false;
     axis[0] = 0;
     axis[1] = 0;
     axis[2] = 0;
     axis[3] = 0;
-	
-	flap = 0;
-	fm = 0;
-	aux = 0;
+
+    flap = 0;
+    fm = 0;
+    aux = 0;
   } else {
     connected = true;
   }
-} 
+}
+
+int t = 0;
+void updateLog() {
+
+  if (!connected) {
+    t++;
+  } else {
+    t = 0;
+  }
+
+  if (t < 50) {
+    Serial2.print(millis());
+    Serial2.print("\t");
+    Serial2.print(connected);
+    Serial2.print("\t");
+    Serial2.print(axis[0]);
+    Serial2.print("\t");
+    Serial2.print(axis[1]);
+    Serial2.print("\t");
+    Serial2.print(axis[2]);
+    Serial2.print("\t");
+    Serial2.print(axis[3]);
+    Serial2.print("\t");
+    Serial2.print(flap);
+    Serial2.print("\t");
+    Serial2.print(aux);
+    Serial2.print("\t");
+    Serial2.print(fm);
+    Serial2.print("\t");
+    Serial2.print(accel_data[0]);
+    Serial2.print("\t");
+    Serial2.print(accel_data[1]);
+    Serial2.print("\t");
+    Serial2.print(accel_data[2]);
+    Serial2.print("\t");
+    Serial2.print(gyro_data[0]);
+    Serial2.print("\t");
+    Serial2.print(gyro_data[1]);
+    Serial2.print("\t");
+    Serial2.print(gyro_data[2]);
+    Serial2.print("\t");
+    for(int8_t i = 0; i < 16; i++) {
+      Serial2.print(motor[i]);
+      Serial2.print("\t");
+    }
+    Serial2.println("");
+  }
+}
 
